094bedb260925f7330ed68f6913ddb094aa00a76
thaisc98 <thaisc98@hotmail.es>
2022-08-31 00:28:53 -0400

    Initial commit

---
+++ src/main/java/org/example/ExerciseLectureOne.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.util.List;
+
+public class ExerciseLectureOne {
+
+    public static void main(String[] args) {
+
+        // Print only odd numbers from the list.
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        oddNumbers(numbers);
+
+        // print all courses individually.
+        List<String> courses = List.of("Spring", "Spring boot", "API", "Microservices", "AMS","PCP","Azure","Docker", "Kubernetes");
+        printIndividually(courses);
+
+        //print courses containing the word "Spring".
+        printWordsSpring(courses);
+
+        //print courses whose name has at least 4 letters.
+        printWordsWithFourLetters(courses);
+
+        // print the cubes of odd numbers
+        printCubesOfOddNumbers(numbers);
+
+        // print the numbers of characters in each course name
+        printNumberOfCharactersCourses(courses);
+    }
+
+
+
+    private static void oddNumbers(List<Integer> numbers){
+        numbers.stream().filter(number -> number % 2 != 0)
+                .forEach(System.out::println);
+    }
+
+    private static void printIndividually(List<String> courses){
+        courses.stream().forEach(System.out::println);
+    }
+
+    private static void printWordsSpring(List<String> courses){
+        courses.stream()
+                .filter(course -> course.contains("Spring"))
+                .forEach(System.out::println);
+    }
+
+    private static void printWordsWithFourLetters(List<String> courses){
+        courses.stream()
+                .filter(course -> course.length() >= 4)
+                .forEach(System.out::println);
+    }
+
+    private static void printCubesOfOddNumbers(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .map(number -> Math.pow(number,3))
+                .forEach(System.out::println);
+    }
+
+    private static void printNumberOfCharactersCourses(List<String> courses) {
+        courses.stream()
+                .map(String::length)
+                .forEach(System.out::println);
+    }
+
+
+}
---
+++ src/main/java/org/example/LectureOne.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.util.List;
+
+public class LectureOne {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+//        printAllNumbersInListStructured(numbers);
+//        printAllNumbersInListFunctional(numbers);
+//        printEvenNumbersInListStructured(numbers);
+//        printAllEvenInListFunctional(numbers);
+        printSquareOfEvenInListFunctional(numbers);
+
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+        //how to loop the numbers ?
+        //structured
+        for(int number: numbers){
+            System.out.println(number);
+        }
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        // what to do ?
+        numbers.stream().forEach(LectureOne::print);// method reference
+        // simple way
+        numbers.stream().forEach(System.out::println);
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        //how to loop the numbers ?
+        //structured
+        for(int number: numbers){
+            if(number % 2 == 0){
+                System.out.println(number);
+            }
+        }
+    }
+
+    private static void print(int number){
+        System.out.println(number);
+    }
+
+    private static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+
+    private static void printAllEvenInListFunctional(List<Integer> numbers) {
+        // what to do ?
+        numbers.stream()
+                .filter(LectureOne::isEven) //filter = only allow even numbers
+                .forEach(System.out::println); // method reference
+        // simple way
+        numbers.stream()
+                .filter(number -> number % 2 == 0) //lambda expression
+                .forEach(System.out::println); // method reference
+    }
+
+    private static void printSquareOfEvenInListFunctional(List<Integer> numbers) {
+        // what to do ?
+        numbers.stream()
+                .filter(number -> number % 2 == 0) //lambda expression
+                .map(number -> number * number)
+                .forEach(System.out::println); // method reference
+    }
+}
